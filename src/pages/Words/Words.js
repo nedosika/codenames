528d96214a3e3cab88f7165916a7f1ad747b748b
nedosika <nedosika@mail.ru>
2021-11-30 00:13:56 +0200

    add actions

--- src/pages/Words/Words.js
+++ src/pages/Words/Words.js
@@ -1,12 +1,17 @@
 import * as React from 'react';
-import {DataGrid} from '@mui/x-data-grid';
+import {DataGrid, useGridApiRef, GridActionsCellItem} from '@mui/x-data-grid';
 
 import Alert from '@mui/material/Alert';
 import {useWords} from "../../hooks/useWords";
 import Layout from "../../layout";
-import Button from "@mui/material/Button";
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Close';
 
 export default function Words() {
+    const apiRef = useGridApiRef();
     const {words} = useWords();
     const [editRowsModel, setEditRowsModel] = React.useState({});
 
@@ -14,10 +19,52 @@ export default function Words() {
         setEditRowsModel(model);
     }, []);
 
+    const columns = [
+        {field: 'value', headerName: 'Word', width: 180, editable: true},
+        {field: 'ru', headerName: 'Russian', editable: true},
+        {
+            field: 'create',
+            headerName: 'Date Created',
+            type: 'date',
+            width: 180,
+            editable: true,
+        },
+        {
+            field: 'edit',
+            headerName: 'Last Edited',
+            type: 'dateTime',
+            width: 220,
+            editable: true,
+        },
+        {
+            field: 'actions',
+            headerName: 'Actions',
+            width: 100,
+            type: 'actions',
+            getActions: ({id}) => {
+                return([
+                    <GridActionsCellItem
+                        icon={<EditIcon />}
+                        label="Edit"
+                        onClick={() => {}}
+                        color="inherit"
+                    />,
+                    <GridActionsCellItem
+                        icon={<DeleteIcon />}
+                        label="Delete"
+                        onClick={() => {}}
+                        color="inherit"
+                    />,
+                ])
+            }
+        },
+    ];
+
     return (
         <Layout title="Words">
             <div style={{height: 'calc(100vh - 95px)', marginTop: 80}}>
                 <DataGrid
+                    apiRef={apiRef}
                     rows={words}
                     columns={columns}
                     editRowsModel={editRowsModel}
@@ -28,36 +75,3 @@ export default function Words() {
         </Layout>
     );
 }
-
-const columns = [
-    {field: 'value', headerName: 'Word', width: 180, editable: true},
-    {field: 'ru', headerName: 'Russian', editable: true},
-    {
-        field: 'create',
-        headerName: 'Date Created',
-        type: 'date',
-        width: 180,
-        editable: true,
-    },
-    {
-        field: 'edit',
-        headerName: 'Last Edited',
-        type: 'dateTime',
-        width: 220,
-        editable: true,
-    },
-    {
-        field: 'actions',
-        headerName: 'Actions',
-        width: 220,
-        type: 'actions',
-        editable: true,
-        getActions: ({id}) => {
-            return([
-                <Button onClick={() => alert(id)}>Save</Button>,
-                <Button onClick={() => alert(id)}>Cancel</Button>
-
-            ])
-        }
-    },
-];
