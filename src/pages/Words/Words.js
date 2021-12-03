55eadecdca662d2040e9c178aebd288b9de6ad4c
NedosikaPavel <pavel.nedosika@gmail.com>
2021-12-03 16:03:12 +0200

    add delete button

--- src/pages/Words/Words.js
+++ src/pages/Words/Words.js
@@ -9,61 +9,35 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 import SaveIcon from '@mui/icons-material/Save';
 import CancelIcon from '@mui/icons-material/Close';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+
+
+function EditToolbar(props) {
+    return (
+        <Box
+            sx={{
+                borderBottom: 1,
+                borderColor: 'divider',
+            }}
+        >
+            <Tooltip title="Delete" sx={{margin: '5px'}}>
+                <IconButton>
+                    <DeleteIcon/>
+                </IconButton>
+            </Tooltip>
+        </Box>
+    );
+}
 
 export default function Words() {
-    const apiRef = useGridApiRef();
     const {words} = useWords();
     const [editRowsModel, setEditRowsModel] = React.useState({});
-    const [selectedCellParams, setSelectedCellParams] = React.useState(null);
-
-    console.log(selectedCellParams)
-
-    const handleCellClick = React.useCallback((params) => {
-        setSelectedCellParams(params);
-    }, []);
 
     const handleEditRowsModelChange = React.useCallback((model) => {
         setEditRowsModel(model);
-    }, []);
-
-    const handleCancelClick = (id) => (event) => {
-        event.stopPropagation();
-        apiRef.current.setRowMode(id, 'view');
-
-        const row = apiRef.current.getRow(id);
-        if (row.isNew) {
-            apiRef.current.updateRows([{ id, _action: 'delete' }]);
-        }
-    };
-
-    const handleEditClick = (id) => (event) => {
-        event.stopPropagation();
-        apiRef.current.setRowMode(id, 'edit');
-    };
-
-    const handleDeleteClick = (id) => (event) => {
-        event.stopPropagation();
-        apiRef.current.updateRows([{ id, _action: 'delete' }]);
-    };
-
-    const handleClick = () => {
-        if (!selectedCellParams) {
-            return;
-        }
-        const { id, field, cellMode } = selectedCellParams;
-        if (cellMode === 'edit') {
-            //apiRef.current.commitCellChange({ id, field });
-            //apiRef.current.setCellMode(id, field, 'view');
-            setSelectedCellParams({ ...selectedCellParams, cellMode: 'view'});
-        } else {
-            //apiRef.current.setCellMode(id, field, 'edit');
-            setSelectedCellParams({ ...selectedCellParams, cellMode: 'edit', hasFocus: true  });
-        }
-    };
-
-    const handleDoubleCellClick = React.useCallback((params, event) => {
-        console.log(params)
-        event.defaultMuiPrevented = true;
     }, []);
 
     const columns = [
@@ -82,67 +56,21 @@ export default function Words() {
             type: 'dateTime',
             width: 220,
             editable: true,
-        },
-        {
-            field: 'actions',
-            headerName: 'Actions',
-            width: 100,
-            type: 'actions',
-            getActions: (action) => {
-                //const { cellMode } = selectedCellParams;
-                //const isInEditMode = cellMode === 'edit' && action.id === id;
-                const isInEditMode = false
-
-                if (isInEditMode) {
-                    return [
-                        <GridActionsCellItem
-                            icon={<SaveIcon />}
-                            label="Save"
-                            onClick={handleClick}
-                            color="primary"
-                        />,
-                        <GridActionsCellItem
-                            icon={<CancelIcon />}
-                            label="Cancel"
-                            onClick={handleClick}
-                            color="inherit"
-                        />,
-                    ];
-                }
-
-                return([
-                    <GridActionsCellItem
-                        icon={<EditIcon />}
-                        label="Edit"
-                        onClick={handleClick}
-                        color="inherit"
-                    />,
-                    <GridActionsCellItem
-                        icon={<DeleteIcon />}
-                        label="Delete"
-                        onClick={handleClick}
-                        color="inherit"
-                    />,
-                ])
-            }
-        },
+        }
     ];
 
     return (
         <Layout title="Words">
             <div style={{height: 'calc(100vh - 95px)', marginTop: 80}}>
                 <DataGrid
-                    apiRef={apiRef}
+                    components={{Toolbar: EditToolbar}}
                     rows={words}
                     columns={columns}
                     editRowsModel={editRowsModel}
                     editMode="row"
+                    disableSelectionOnClick
                     onEditRowsModelChange={handleEditRowsModelChange}
-                    // onRowSelected={(parmas) => {
-                    //     console.log(Array.from(parmas.api.current.getSelectedRows().entries()));
-                    // }}
-                    onCellDoubleClick={handleDoubleCellClick}
-                    onCellClick={handleCellClick}
+                    checkboxSelection
                 />
             </div>
         </Layout>
