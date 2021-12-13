7dc6469ed1f41d8e381dd1064d49e9b440c89d3e
nedosika <nedosika@gmail.com>
2021-12-13 15:31:21 +0200

    add regenerate game

--- src/pages/Boards/Boards.js
+++ src/pages/Boards/Boards.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
-import {experimentalStyled as styled} from '@mui/material/styles';
-
 import Layout from "../../layout";
-import background from "../../assets/images/board.png";
 import red from "../../assets/images/red.png";
 import blue from "../../assets/images/blue.png";
 import black from "../../assets/images/black.png";
 import yellow from "../../assets/images/neutral.png";
-import blueGame from "../../assets/images/blue_game.png"
-import redGame from "../../assets/images/red_game.png"
+import background from "../../assets/images/board.png";
+import redGame from "../../assets/images/red_game.png";
+import blueGame from "../../assets/images/blue_game.png";
+
+import Box from "@mui/material/Box";
+import Fab from "@mui/material/Fab";
+import AutorenewIcon from "@mui/icons-material/Autorenew";
+import {experimentalStyled as styled} from '@mui/material/styles';
 
 const COLORS = [red, blue, yellow, black];
 
@@ -19,6 +22,17 @@ const Item = styled('div')(({background}) => ({
     backgroundSize: "contain",
     height: 65,
     width: 65,
+}));
+
+const GameLight = styled('div')(({sx, background}) => ({
+    backgroundImage: `url(${background})`,
+    backgroundSize: "contain",
+    backgroundRepeat: "no-repeat",
+    boxSizing: 'border-box',
+    height: '57px',
+    width: '19px',
+    position: "absolute",
+    ...sx
 }));
 
 const Board = styled('div')(() => ({
@@ -33,63 +47,66 @@ const Board = styled('div')(() => ({
     backgroundSize: "contain",
     boxSizing: 'border-box',
     padding: 65
-}))
+}));
+
+const ButtonsPanel = styled(Box)(() => ({
+    position: "fixed",
+    bottom: 16,
+    right: 16,
+    '& > :not(style)': {m: 1}
+}));
 
 const Boards = () => {
-    const board = [3, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1];
-    const isBlueGame = Math.random() > 0.5;
-    board.push(Number(isBlueGame));
-    board.sort((a, b) => 0.5 - Math.random());
+    const [board, setBoard] = React.useState({
+        items: [],
+        gameColor: null
+    });
+
+    const initBoard = () => {
+        const board = [3, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1];
+        const owner = Number(Math.random() > 0.5);
+        const gameColor = owner ? blueGame : redGame;
+
+        board.push(owner);
+        board.sort((a, b) => 0.5 - Math.random());
+        setBoard({
+            items: [...board],
+            gameColor
+        });
+    }
+
+    React.useEffect(() => {
+        initBoard();
+    }, [])
 
     return (
         <Layout title='Boards generator'>
             <Board>
-                <div style={{
-                    backgroundImage: `url(${isBlueGame ? blueGame : redGame})`,
-                    backgroundSize: "contain",
-                    backgroundRepeat: "no-repeat",
-                    boxSizing: 'border-box',
-                    height: '57px',
-                    width: '19px',
-                    position: "absolute",
-                    marginRight: '472px'
-                }}/>
-                <div style={{
-                    backgroundImage: `url(${isBlueGame ? blueGame : redGame})`,
-                    backgroundSize: "contain",
-                    backgroundRepeat: "no-repeat",
-                    boxSizing: 'border-box',
-                    height: '57px',
-                    width: '19px',
-                    position: "absolute",
-                    marginLeft: '469px'
-                }}/>
-                <div style={{
-                    backgroundImage: `url(${isBlueGame ? blueGame : redGame})`,
-                    backgroundSize: "contain",
-                    backgroundRepeat: "no-repeat",
-                    boxSizing: 'border-box',
-                    height: '57px',
-                    width: '19px',
-                    position: "absolute",
-                    marginBottom: '472px',
-                    transform: 'rotate(90deg)'
-                }}/>
-                <div style={{
-                    backgroundImage: `url(${isBlueGame ? blueGame : redGame})`,
-                    backgroundSize: "contain",
-                    backgroundRepeat: "no-repeat",
-                    boxSizing: 'border-box',
-                    height: '57px',
-                    width: '19px',
-                    position: "absolute",
-                    marginTop: '467px',
-                    transform: 'rotate(90deg)'
-                }}/>
+                <GameLight
+                    sx={{marginRight: '472px'}}
+                    background={board.gameColor}
+                />
+                <GameLight
+                    sx={{marginLeft: '469px'}}
+                    background={board.gameColor}
+                />
+                <GameLight
+                    sx={{marginBottom: '472px', transform: 'rotate(90deg)'}}
+                    background={board.gameColor}
+                />
+                <GameLight
+                    sx={{marginTop: '467px', transform: 'rotate(90deg)'}}
+                    background={board.gameColor}
+                />
                 {
-                    board.map((item) => <Item background={COLORS[item]}/>)
+                    board?.items.map((item) => <Item background={COLORS[item]}/>)
                 }
             </Board>
+            <ButtonsPanel>
+                <Fab color="primary" onClick={initBoard}>
+                    <AutorenewIcon/>
+                </Fab>
+            </ButtonsPanel>
         </Layout>
     );
 };
--- src/pages/Boards/ButtonsPanel.js
+++ src/pages/Boards/ButtonsPanel.js
@@ -1,33 +1,18 @@
 import React from 'react';
-import {useNavigate} from "react-router-dom";
 
 import Box from "@mui/material/Box";
-import Fab from "@mui/material/Fab";
-import AddIcon from '@mui/icons-material/Add';
-import AutorenewIcon from '@mui/icons-material/Autorenew';
 
-const ButtonsPanel = ({onResetGame}) => {
-    const navigate = useNavigate();
-
-    const handleOpenCreateProcessDialog = () => {
-        navigate(`add`);
-    };
-
-        return (
-            <Box sx={{
-                position: "fixed",
-                bottom: 16,
-                right: 16,
-                '& > :not(style)': {m: 1}
-            }}>
-                <Fab color="primary" onClick={onResetGame}>
-                    <AutorenewIcon/>
-                </Fab>
-                <Fab color="primary" onClick={handleOpenCreateProcessDialog}>
-                    <AddIcon/>
-                </Fab>
-            </Box>
-        );
+const ButtonsPanel = ({children}) => {
+    return (
+        <Box sx={{
+            position: "fixed",
+            bottom: 16,
+            right: 16,
+            '& > :not(style)': {m: 1}
+        }}>
+            {children}
+        </Box>
+    );
 };
 
 export default ButtonsPanel;
